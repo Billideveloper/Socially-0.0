e97f49b47ca61a4080e7ab5706a6be926df63f2d
Ravi <thakurravi450@gmail.com>
2020-04-04 17:01:37 +0530

    Initial Commit

---
+++ SStories/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SStories
+//
+//  Created by Ravi Thakur on 04/04/20.
+//  Copyright © 2020 Ravi Thakur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
